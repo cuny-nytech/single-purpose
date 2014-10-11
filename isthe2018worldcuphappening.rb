cfa54a67f64c5bc1992a85938fd80879b40e6258
Danielle Zoe Aloicius <daniez@users.noreply.github.com>
2014-10-11 02:09:35 -0400

    ran rubocop

--- isthe2018worldcuphappening.rb
+++ isthe2018worldcuphappening.rb
@@ -2,13 +2,10 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-	
-if Date.parse('2018-6-8')>Date.today
-   	   erb :nyet
-   else
-   	   erb :da		   
-   end
+
+  if Date.parse('2018-6-8') > Date.today
+    erb :nyet
+  else
+    erb :da		   
+  end
 end
-
-
-
