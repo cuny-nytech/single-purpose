18b0ee6f34bea45efcaf2beed82131ffe3a42f76
Slavisa Djukic <Slavisa.Djukic15@myhunter.cuny.edu>
2014-10-16 23:32:03 -0400

    Working app. Could remove duplication in api_call function by moving it out in module

--- weather.rb
+++ weather.rb
@@ -3,6 +3,7 @@ require 'sinatra/base'
 require 'json'
 require 'httparty'
 require 'data_mapper'
+#require 'sinatra/config_file'
 
 # This is way to pass secret arguments to program 
 # or as environment variable
@@ -31,6 +32,42 @@ DataMapper.finalize.auto_upgrade!
 
 class App < Sinatra::Base
 
+
+  def self.initialize_database
+    cities = ["New_York", "San_Francisco", "Chicago"]
+    states = ["NY", "CA", "IL"]
+
+    3.times do |i|
+      c = WeatherData.new
+      c.id = i
+      c.city = cities[i].tr('_', ' ')
+      c.state = states[i].tr('_', ' ')
+      self.make_api_call(c, states[i], cities[i])
+    end
+
+  end
+
+
+  def self.make_api_call(db_record, state, city)
+    query = "http://api.wunderground.com/api/"+API_KEY+"/conditions/q/#{state}/#{city}.json"
+    res = HTTParty.get(query)
+    parsed = JSON.parse(res.body)
+    data = parsed['current_observation']
+    db_record.humidity = data['relative_humidity']
+    db_record.temperature = data['temp_f']
+    db_record.icon = data['icon']
+    db_record.updated_at = Time.now
+    db_record.save
+  end
+
+
+  # make sure we have something in the database
+  configure do
+    #config_file 'conf.yaml'
+    data = WeatherData.all
+    self.initialize_database if data.size == 0
+  end
+
   get '/' do
     @conditions = WeatherData.all :order => :id.desc
     update @conditions if update? @conditions
@@ -38,15 +75,10 @@ class App < Sinatra::Base
     erb :home
   end
 
-  helpers  do
+  helpers do
     def update cities
       cities.each do |city|
-        #query = "http://api.wunderground.com/api/"+API_KEY+"/conditions/q/#{city.state}/#{city.city}.json"
-        #res = HTTParty.get(query)
-        #parsed = JSON.parse(res.body)
-        #data = parsed['current_observation']
-        city.updated_at = Time.now
-        city.save
+        update_db(city, city.state, city.city.tr(' ', '_'))
       end
     end
   
@@ -57,7 +89,19 @@ class App < Sinatra::Base
         latest = latest > now - city.updated_at.to_time ? latest : now - city.updated_at.to_time
       end
       
-      latest > 120
+      latest > 300
+    end
+
+    def update_db(db_record, state, city)
+      query = "http://api.wunderground.com/api/"+API_KEY+"/conditions/q/#{state}/#{city}.json"
+      res = HTTParty.get(query)
+      parsed = JSON.parse(res.body)
+      data = parsed['current_observation']
+      db_record.humidity = data['relative_humidity']
+      db_record.temperature = data['temp_f']
+      db_record.icon = data['icon']
+      db_record.updated_at = Time.now
+      db_record.save
     end
   end
 
