3840e27156275ce8328936f25d80f1f71e95057a
Danielle Zoe Aloicius <daniez@users.noreply.github.com>
2014-10-19 14:44:10 -0400

    this is a test

--- isthe2018worldcuphappening.rb
+++ isthe2018worldcuphappening.rb
@@ -3,10 +3,5 @@ require 'sinatra'
 
 get '/' do
 
-  if Date.parse('2018-6-8') > Date.today
-  #if Date.parse('2014-10-14') > Date.today
-    erb :nyet
-  else
-    erb :da
-  end
+ "Hello this is a test"
 end
