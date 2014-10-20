4859a75de1d0b36ee79a9bc20efbf2fe7bbaff11
Danielle Zoe Aloicius <daniez@users.noreply.github.com>
2014-10-20 01:45:02 -0400

    change isthe2018worldcuphappening back

--- isthe2018worldcuphappening.rb
+++ isthe2018worldcuphappening.rb
@@ -3,5 +3,10 @@ require 'sinatra'
 
 get '/' do
 
- "Hello this is a test"
+  if Date.parse('2018-6-8') > Date.today
+  #if Date.parse('2014-10-14') > Date.today
+    erb :nyet
+  else
+    erb :da
+  end
 end
